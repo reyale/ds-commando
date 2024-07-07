76655968269abde35d72404130b57cb012c2e703
alex.reynolds <windfinder@gmail.com>
2024-07-07 13:47:45 -0400

    more tests

--- test.sh
+++ test.sh
@@ -8,4 +8,7 @@ diff ./output/test1.csv ./expected/test1.csv
 ./merge ./data/people1.csv ./data/people2.csv ./data/people1.csv > ./output/test2.csv
 diff ./output/test2.csv ./expected/test2.csv
 
+cat ./data/people1.csv | ./merge ./data/people2.csv ./data/people1.csv > ./output/test3.csv
+diff ./output/test2.csv ./expected/test2.csv
+
 echo "test complete - there should be no displayed differences if successful"
