d8529cca398e3ee8603f498f2b748403d3f339fc
alex.reynolds <windfinder@gmail.com>
2024-07-07 13:46:30 -0400

    more test

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # test 1
-
 ./merge ./data/people1.csv ./data/people2.csv > ./output/test1.csv
 diff ./output/test1.csv ./expected/test1.csv
+
+# test 2
+./merge ./data/people1.csv ./data/people2.csv ./data/people1.csv > ./output/test2.csv
+diff ./output/test2.csv ./expected/test2.csv
 
 echo "test complete - there should be no displayed differences if successful"
